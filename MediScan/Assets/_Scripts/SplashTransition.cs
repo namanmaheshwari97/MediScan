95edbcff44be3264bbf4ffaa61cb67699b3052a8
namanmaheshwari97 <namanmaheshwari97@gmail.com>
2016-10-03 15:57:35 +0530

    Pushed MediScan Unity

---
+++ MediScan/Assets/_Scripts/SplashTransition.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class SplashTransition : MonoBehaviour {
+
+	private float remainingtime;
+	void Start () {
+		remainingtime = 3;
+	}
+
+	void Update(){
+		remainingtime--;
+		if (remainingtime == 0) {
+			changeScene ();
+		}
+	}
+
+	void changeScene(){
+		SceneManager.LoadScene (1);
+	}
+}
