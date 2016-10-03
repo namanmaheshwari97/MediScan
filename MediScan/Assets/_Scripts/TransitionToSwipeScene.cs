43892e789779be6cab7b2ed96be41e4047c2be5c
namanmaheshwari97 <namanmaheshwari97@gmail.com>
2016-10-03 15:37:07 +0530

    Added MediScan Unity folder

---
+++ MediScan/Assets/_Scripts/TransitionToSwipeScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class TransitionToswipe : MonoBehaviour {
+
+
+	void Start () {
+		changeScene ();
+	}
+	void changeScene(){
+		SceneManager.GetSceneAt (1);
+	}
+}
