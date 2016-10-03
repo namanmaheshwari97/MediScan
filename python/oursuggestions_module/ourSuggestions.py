bbc5cecba74beda80578d8b089c3be4c1620e873
arp1561 <joshiarpit2@gmail.com>
2016-10-03 17:05:09 +0530

    Modified oursuggestions_module

---
+++ python/oursuggestions_module/ourSuggestions.py
@@ -0,0 +1,12 @@
+import pandas as pd
+def suggest(recvAilment):
+	data=pd.read_csv("suggested_medicines_database.txt",delim_whitespace="",header=None)
+	ailment=data[0]
+	firstMedicine=data[1]
+	secondMedicine=data[2]
+	for i in range(len(ailment)):
+		if(recvAilment==ailment[i]):
+			finalMedicineString = firstMedicine[i]+";"+secondMedicine[i]
+			return finalMedicineString
+
+
