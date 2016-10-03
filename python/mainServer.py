68fa6212ba8228d66741473efad116b14c18c6a7
arp1561 <joshiarpit2@gmail.com>
2016-10-03 18:54:43 +0530

    Modified mainServer

--- python/mainServer.py
+++ python/mainServer.py
@@ -1,4 +1,6 @@
+
 import sys
+import socket
 sys.path.insert(0,'sos_module/')
 sys.path.insert(0,'homeRemedies_module/')
 sys.path.insert(0,'oursuggestions_module/')
@@ -8,3 +10,21 @@ import sosmessage as sm
 import homeRemedies as hr
 import ourSuggestions as oursug
 import recommendationEngine as re
+
+
+
+s = socket.socket()
+host = "10.4.59.36"
+port = 8081
+s.bind((host,port))
+
+s.listen(5)
+s.settimeout(300.0)
+
+while True:
+    c,addr = s.accept()
+    print "Connected"
+    c.send("Got it bro")
+    c.close()
+    break
+
