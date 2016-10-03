ec05b761fabdf8d4501eb801e0d9eeb5db553c64
namanmaheshwari97 <namanmaheshwari97@gmail.com>
2016-10-03 17:02:41 +0530

    Updated MediScan Unity at 5pm on 3/10

---
+++ MediScan/Assets/_Scripts/HomeRemediesBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class HomeRemediesBehaviour : MonoBehaviour {
+
+	private Button HRbutton;
+	public Canvas AR_MainCanv;
+	public Canvas AR_HRCanv;
+	// Use this for initialization
+	void Start () {
+		AR_HRCanv.isActiveAndEnabled (false);
+		HRbutton = this.GetComponent<Button> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HRbutton.onClick.AddListener (() => getCanvas ());
+	}
+	
+	void getCanvas(){
+		AR_HRCanv.istransparent
+	}
+}
