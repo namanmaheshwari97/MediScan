c5c01d7e86e01d7dd010b7e8eab0c777b7e05cac
arp1561 <joshiarpit2@gmail.com>
2016-10-03 18:41:19 +0530

    Added mainServer file for comms

---
+++ python/mainServer.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.insert(0,'sos_module/')
+sys.path.insert(0,'homeRemedies_module/')
+sys.path.insert(0,'oursuggestions_module/')
+sys.path.insert(0,'recommender_module/')
+
+import sosmessage as sm
+import homeRemedies as hr
+import ourSuggestions as oursug
+import recommendationEngine as re
