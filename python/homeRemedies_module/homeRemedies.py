916d150b65ab0dce654d35f2e9fcc9dfe4098673
arp1561 <joshiarpit2@gmail.com>
2016-10-03 15:50:15 +0530

    Added Home remedies Module

---
+++ python/homeRemedies_module/homeRemedies.py
@@ -0,0 +1,15 @@
+homeRemedies_dict = {
+                        'loose motions':"Lemon juice has anti inflammatory properties that will help in easily clearing the stomach. Extract the juice of lemon and make lemonade. Drink it whenever necessary.",
+                        'sinusitus':"Hot water vapor can help moisten the sinuses. Sprinkle a few drops of eucalyptus or menthol in the shower and steam up your bathroom.",
+                        'nausea':"One of the oldest and perhaps the best remedy for nausea is ginger. Try a warm cup of ginger tea.",
+                        'strains or sprains':"Rest the strained or sprained muscle. Ice it after every 20 minutes if possible.",
+                        'allergy':"Turmeric, a spice common in curry and Indian cuisine, contains curcumin. It may act as a decongestant, help reduce allergy symptoms",
+                        'cold and flu':"One of these classic home remedies for colds is comprised of three familiar ingredients-garlic, lemon, and honey. You can combine them in more involved ways if you wish.",
+                        'headache':"Extract mint juice from a handful of mint leaves and apply it on forehead and temples to treat headache.",
+                        'acid reflux':"A spoonful of sodium bicarbonate, or teaspoon-full to be exact, can help put an end to the gnawing, burning, sensation of heartburn caused by acid reflux."
+                    }
+def getHomeRemedies(ailment):
+    return homeRemedies_dict[ailment]
+        
+        
+        
