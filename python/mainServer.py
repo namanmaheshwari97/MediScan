8384b130ed72085f68c14c06ccd5253996f3f814
arp1561 <joshiarpit2@gmail.com>
2016-10-04 06:21:49 +0530

    Modified main server

--- python/mainServer.py
+++ python/mainServer.py
@@ -14,13 +14,14 @@ import recommendationEngine as re
 
 s = socket.socket()
 host = "10.4.59.36"
-port = 8101
+port = 8137
 s.bind((host,port))
-
 s.listen(5)
 print "Listening"
 while True:
+    print "Loop started"
     c,addr = s.accept()
+    print "Connected"
     recvString = c.recv(1024)
     parsedString = recvString.split(";")
     if parsedString[0] == "sos":
@@ -29,18 +30,23 @@ while True:
         break
     
     elif parsedString[0] == "home remedies":
-        toSendHomeRemedies = hr.getHomeRemedies(parsedString[1])
-        c.send(toSendHomeRemedies)              
+        print "home remedies called"
+        ailment = parsedString[1]
+        ailment = ailment[:-1]
+        toSendHomeRemedies = hr.getHomeRemedies(ailment)
+        toSendHomeRemedies = toSendHomeRemedies + "\n"
+        c.send(toSendHomeRemedies)
     
     elif parsedString[0] == "check":
         toSendCheck = re.parseString(parsedString[1]+";"+parsedString[2])
+        toSendCheck = toSendCheck + "\n"
         c.send(toSendCheck)
     
-    elif parsedString[0] == "our suggestions"
+    elif parsedString[0] == "our suggestions":
        toSendMedicines = oursug.suggest(parsedString[1])
        c.send(toSendMedicines)
     
-    elif parsedString[0] == "exit"
+    elif parsedString[0] == "exit":
         print "Exiting and closing connections"
         c.close()
         break
